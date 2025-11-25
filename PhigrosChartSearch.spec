951a8c4152260f1aa40eb644ee637ee2c09444de
xiaoxiaoqwq647 <1463198460@qq.com>
2025-11-25 21:37:18 +0800

    PhigrosChartSearch Ver.1.1 更新了1种更快的搜索模式。

--- PhigrosChartSearch.spec
+++ PhigrosChartSearch.spec
@@ -2,7 +2,7 @@
 
 
 a = Analysis(
-    ['code\\phigroschart.py'],
+    ['code\\PhigrosChartSearch.py'],
     pathex=[],
     binaries=[],
     datas=[],
@@ -21,7 +21,7 @@ exe = EXE(
     a.scripts,
     [],
     exclude_binaries=True,
-    name='phigroschart',
+    name='PhigrosChartSearch',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
@@ -40,5 +40,5 @@ coll = COLLECT(
     strip=False,
     upx=True,
     upx_exclude=[],
-    name='phigroschart',
+    name='PhigrosChartSearch',
 )
--- code/PhigrosChartSearch.py
+++ code/PhigrosChartSearch.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 
 def chart_mode1(name):
     file = open(name,mode='r',encoding="utf-8")
@@ -25,11 +26,23 @@ def chart_mode2(name):
     file.close()
     return notes_chart
 
+def chart_mode3(name):
+    file = open(name,mode='r',encoding="utf-8")
+    res = file.readline()
+    notes_chart = 0
+    res_python = json.loads(res)
+    judgelines = res_python["judgeLineList"]
+    for judgeline in judgelines:
+        notes_chart += (len(judgeline["notesAbove"]) + len(judgeline["notesBelow"]))
+    file.close()
+    return notes_chart
 
-print("Phigros Chart Search V1.0")
+
+print("Phigros Chart Search V1.1")
 charts_directory = input("Please enter the directory of the charts(Empty input means current directory):")
 print("Mode 1:By each notes total")
 print("Mode 2:By total notes")
+print("Mode 3:By total notes(Using JSON parsing)(Recommended)")
 mode = input("Please select mode:")
 if mode == "1":
     print("Please enter the number of notes which chart you want to search.")
@@ -41,6 +54,7 @@ if mode == "1":
     ans = []
     difficulty_dict = {1:"EZ",2:"HD",3:"IN",4:"AT",5:"Legacy"}
     notes_input = [tap,drag,hold,flick]
+    count = 0
     difficulty_num = input("Please choose the difficulty of the chart(enter number) EZ-1 HD-2 IN-3 AT-4 Legacy-5 :")
     if difficulty_num != "1" and difficulty_num != "2" and difficulty_num != "3" and difficulty_num != "4" and difficulty_num != "5":
         print("Your input is wrong!")
@@ -54,6 +68,9 @@ if mode == "1":
                 notes_true = chart_mode1(charts_directory_one)
                 if notes_input == notes_true:
                     ans.append(i)
+            count += 1
+            if count % 10 == 0:
+                print(f"Processed {count}/{len(list)} charts...")
         print(ans)
 elif mode == "2":
     print("Please enter the number of notes which chart you want to search.")
@@ -61,6 +78,7 @@ elif mode == "2":
     notes_input = int(input("Total notes:"))
     ans = []
     difficulty_dict = {1:"EZ",2:"HD",3:"IN",4:"AT",5:"Legacy"}
+    count = 0
     difficulty_num = input("Please choose the difficulty of the chart(enter number) EZ-1 HD-2 IN-3 AT-4 Legacy-5 :")
     if difficulty_num != "1" and difficulty_num != "2" and difficulty_num != "3" and difficulty_num != "4" and difficulty_num != "5":
         print("Your input is wrong!")
@@ -74,6 +92,33 @@ elif mode == "2":
                 notes_true = chart_mode2(charts_directory_one)
                 if notes_input == notes_true:
                     ans.append(i)
+            count += 1
+            if count % 10 == 0:
+                print(f"Processed {count}/{len(list)} charts...")
+        print(ans)
+elif mode == "3":
+    print("Please enter the number of notes which chart you want to search.")
+    print("For notes number,you can watch https://www.bilibili.com/video/BV1P1ZsYaEyS/ to find out.")
+    notes_input = int(input("Total notes:"))
+    ans = []
+    difficulty_dict = {1:"EZ",2:"HD",3:"IN",4:"AT",5:"Legacy"}
+    count = 0
+    difficulty_num = input("Please choose the difficulty of the chart(enter number) EZ-1 HD-2 IN-3 AT-4 Legacy-5 :")
+    if difficulty_num != "1" and difficulty_num != "2" and difficulty_num != "3" and difficulty_num != "4" and difficulty_num != "5":
+        print("Your input is wrong!")
+    else:
+        print("Please wait...(maybe more than 1 minutes)")
+        difficulty = difficulty_dict[int(difficulty_num)]
+        list = os.listdir(path = charts_directory)
+        for i in list:
+            if difficulty in i:
+                charts_directory_one = charts_directory + "\\" + i
+                notes_true = chart_mode3(charts_directory_one)
+                if notes_input == notes_true:
+                    ans.append(i)
+            count += 1
+            if count % 10 == 0:
+                print(f"Processed {count}/{len(list)} charts...")
         print(ans)
 else:
     print("Your input is error!")
